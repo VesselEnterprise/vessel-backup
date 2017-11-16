59534e9265a55cbdfd94a69d65082429bd06210e
Unknown <reminisc3@gmail.com>
2017-11-15 22:14:10 -0500

    Added filter iterator class

--- tests/db/db_test.cpp
+++ tests/db/db_test.cpp
@@ -14,6 +14,15 @@ int main(int c, char** argv)
     std::cout << "Get local setting" << std::endl;
     std::cout << db->get_setting_int("compression_level") << std::endl;
 
+    std::cout << "Update extension count" << std::endl;
+    std::cout << db->update_ext_count(".jpg", 100 ) << std::endl;
+
+    std::cout << "Update setting" << std::endl;
+    std::cout << db->update_setting<std::string>("master_server", "backup.brightview.com") << std::endl;
+
+    std::cout << "Test Ignore Ext" << std::endl;
+    std::cout << db->is_ignore_ext(".mp3") << std::endl;
+
     delete db;
 
     return 0;
