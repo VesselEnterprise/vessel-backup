fa56550b13c0170ffccfb97447324a8876b062d3
Unknown <reminisc3@gmail.com>
2019-02-10 00:48:18 -0500

    Update .gitignore

--- server/http.old/core/ui_includes.php
+++
@@ -1,4 +0,0 @@
-<?php
-//ui_includes.php
-
-?>
--- server/http.old/tests/api_exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../core/api_exception.class.php';
-
-throw new APIException('Some weird error');
-
-echo "Some other output that should not be printed";
-
-?>
